c3c0be09e4573b95f980de7b1d5f568dc06c5fcf
Matheus Alcantara <matheusssilv97@gmail.com>
2021-03-23 15:40:07 -0300

    fish: add fish config

---
+++ config/fish/config.fish
@@ -0,0 +1,28 @@
+function fish_prompt
+    if test -n "$SSH_TTY"
+        echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
+    end
+
+    echo -n (set_color blue)(prompt_pwd)' '
+    echo -n (set_color --bold)(fish_git_prompt)' '
+
+    set_color -o
+    if test "$USER" = 'root'
+        echo -n (set_color red)'# '
+    end
+    echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+    set_color normal
+end
+
+function fish_user_key_bindings
+  # ctrl-del
+  bind \e\[3\;5~ kill-word
+
+  # Load bindings to specific machines
+  if functions -q local_bindings
+		local_bindings
+	end
+end
+
+function fish_greeting
+end
