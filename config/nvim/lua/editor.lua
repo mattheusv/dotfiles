53391bc1431b0384a6bbdfffc91dab99b07428ac
Matheus Alcantara <mths.dev@pm.me>
2022-02-14 14:07:01 -0300

    nvim: add SaveSession command

--- config/nvim/lua/editor.lua
+++ config/nvim/lua/editor.lua
@@ -209,6 +209,7 @@ end
 local function configure_commands()
     vim.api.nvim_command("command! CopyBuffer let @+ = expand('%:s')")
     vim.api.nvim_command("command! CopyPkg let @+ = expand('%:h')")
+    vim.api.nvim_command("command! SaveSession :mksession! session.vim")
 end
 
 local function configure_treesitter()
