4645dd82c9ce133d70426e2aaeb33a646d6384b7
Matheus Alcantara <mths.dev@pm.me>
2022-04-18 16:15:45 -0300

    nvim: update nvim-treesitter ensure_installed deprecated option

--- h/.config/nvim/lua/editor.lua
+++ h/.config/nvim/lua/editor.lua
@@ -222,7 +222,7 @@ end
 
 local function configure_treesitter()
     require'nvim-treesitter.configs'.setup {
-        ensure_installed = "maintained",
+        ensure_installed = "all",
         highlight = {
             enable = true,
         },
