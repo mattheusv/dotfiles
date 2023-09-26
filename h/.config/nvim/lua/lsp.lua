9f5b212536518a49c3821d98952df7cbc816a441
Matheus Alcantara <mths.dev@pm.me>
2023-09-26 11:25:15 -0300

    nvim: use telescope to list definitions

--- h/.config/nvim/lua/lsp.lua
+++ h/.config/nvim/lua/lsp.lua
@@ -74,7 +74,7 @@ local on_attach = function(client, bufnr)
 
     -- Mappings.
     buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'gd', [[<cmd>lua require('telescope.builtin').lsp_definitions()<CR>]], opts)
     buf_set_keymap('n', '<leader>gd', '<Cmd>vsplit<cr>:lua vim.lsp.buf.definition()<CR>', opts)
     buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
     buf_set_keymap('n', 'td', [[<cmd>lua require('telescope.builtin').lsp_type_definitions()<CR>]], opts)
