c28d1d823e3907684a6f3be093d22256abf192be
Matheus Alcantara <mths.dev@pm.me>
2024-01-29 15:52:12 -0300

    nvim: add new CopyFile command

--- h/.config/nvim/lua/editor.lua
+++ h/.config/nvim/lua/editor.lua
@@ -199,6 +199,7 @@ end
 local function configure_commands()
     vim.api.nvim_command("command! CopyBuffer execute('lua Copy_buffer()')")
     vim.api.nvim_command("command! CopyPkg let @+ = expand('%:h')")
+    vim.api.nvim_command("command! CopyFile let @+ = expand('%')")
     vim.api.nvim_command("command! SaveSession :mksession! session.vim")
     vim.api.nvim_command("command! OpenSession :source session.vim")
     vim.api.nvim_command("command! Cb :up | %bd | e#")
